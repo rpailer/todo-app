6f0b986f739f0e2bac41d05dee1989759066d72a
mischramm <michaeltschrammel@gmail.com>
2023-05-13 15:08:27 +0200

    Change in NavBar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -49,7 +49,7 @@ export default function NavBar () {
                   
                 </Menu> 
                 <Typography variant="h6">
-                TODO App
+                TODO App of Michael Schammel
               </Typography>
             </Toolbar>
           </AppBar>

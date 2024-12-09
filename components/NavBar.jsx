898baee27f7c9e5843e123ae762d5e9c4e25857c
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2024-12-09 11:35:22 +0100

    trigger deployment

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -49,7 +49,7 @@ export default function NavBar () {
                   
                 </Menu> 
                 <Typography variant="h6">
-                TODO App Rudi
+                TODO App Rudi 2
               </Typography>
             </Toolbar>
           </AppBar>

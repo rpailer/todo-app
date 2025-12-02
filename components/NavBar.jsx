ab8693b8e64532fec2ebb10c07b333f184d79b5a
Rudolf Pailer <rudolf.pailer@at.ibm.com>
2025-12-02 18:44:35 +0100

    Name

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -49,7 +49,7 @@ export default function NavBar () {
                   
                 </Menu> 
                 <Typography variant="h6">
-                TODO App Rudi
+                TODO App Rudolf
               </Typography>
             </Toolbar>
           </AppBar>
